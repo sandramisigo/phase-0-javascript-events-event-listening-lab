68c07fae2040f8588e0cadcced3186f598ba9598
Sandra Misigo <misigosandra@gmail.com>
2024-12-07 15:32:56 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//adding event listeners
 function addingEventListener() {
-}
+        const input = document.getElementById('button');
+        
+        function clickAlert() {
+          alert('I was clicked!');
+        }
+        
+        input.addEventListener('click', clickAlert);
+      }
+      
+ // Calling function to activate event listener
+      addingEventListener();
+       const input = document.getElementById('button');
